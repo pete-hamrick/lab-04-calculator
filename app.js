f6e570e9697a894c911705cc8c71c284d8665f25
Pete Hamrick <pete.hamrick@gmail.com>
2021-06-24 13:26:52 -0700

    added subtract UI and button listener

--- app.js
+++ app.js
@@ -2,10 +2,23 @@
 import { add } from './calculations.js';
 
 // reference needed DOM elements
+//Add
 const addInput1 = document.getElementById('add-num1');
 const addInput2 = document.getElementById('add-num2');
 const addAnswerSpan = document.getElementById('add-answer');
 const addButton = document.getElementById('add-button');
+
+//Subtraction
+const subInput1 = document.getElementById('sub-num1');
+const subInput2 = document.getElementById('sub-num2');
+const subAnswerSpan = document.getElementById('sub-answer');
+const subButton = document.getElementById('sub-button');
+console.log(subInput1, subInput2, subAnswerSpan, subButton);
+
+//Multiplication
+
+//Division
+
 
 // set event listeners 
 addButton.addEventListener('click', ()=> {
@@ -14,6 +27,10 @@ addButton.addEventListener('click', ()=> {
     const addAnswer = add(add1, add2);
     console.log(addAnswer);
     addAnswerSpan.textContent = addAnswer;
+});
+
+subButton.addEventListener('click', () => {
+    console.log('sub button clicked');
 });
   // get user input(s)
   // do any needed work with the value(s)
